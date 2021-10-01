28f940a4d8c92121eb558812a471ba1e36804bd9
ZeroJChang <jdeleonchang@gmail.com>
2021-10-01 13:29:22 -0600

    Cree las interfaces para poder ser generico

---
+++ ClassLibrary/Encryptors/CrypCesar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ClassLibrary.Interfaces;
+using Microsoft.VisualBasic.CompilerServices;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+
+
+
+
+namespace ClassLibrary.Encryptors
+{
+    class CrypCesar<T>
+    {
+    }
+}
---
+++ ClassLibrary/Interfaces/IKeyHolder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Interfaces
+{
+    public interface IKeyHolder 
+    {
+        string GetCesarKey();
+    }
+}
