25721f4f554675a70b398636eeb27c63ef6862a6
Ghandalf312 <djmoralesm@correo.url.edu.gt>
2021-10-09 19:39:34 -0600

    Cifrado y descrifado sdes en api hecho, pendiente algunos cambios en la ruta

--- API/Controllers/CipherController.cs
+++ API/Controllers/CipherController.cs
@@ -2,12 +2,15 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
+using ClassLibrary.Encryptors;
 
 namespace API.Controllers
 {
@@ -50,6 +53,36 @@ namespace API.Controllers
             }
         }
 
+        [Route("sdes/cipher")]
+        [HttpPost]
+        public FileResult CipherSDES([FromForm] IFormFile file, string name, [FromForm] KeyHolder key)
+        {
+            try
+            {
+                var sdes = new SDES<KeyHolder>();
+                if (!KeyHolder.CheckKeyValidness("sdes", key))
+                {
+                    return null;
+                }
+                var originalFileName = file.FileName.Split(".");
+                string originalName = originalFileName[0];
+                var reader = new StreamReader(file.OpenReadStream());
+                string text = reader.ReadToEnd();
+                reader.Close();
+
+                string cipher = sdes.EncryptString(text, key);
+
+                byte[] array = Encoding.UTF8.GetBytes(cipher);
+
+                return base.File(array, "Ciphers/sdes", originalName + ".sdes");
+
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
         [Route("decipher")]
         [HttpPost]
         public async Task<IActionResult> Decipher([FromForm] IFormFile file, [FromForm] KeyHolder key)
@@ -67,6 +100,37 @@ namespace API.Controllers
             catch
             {
                 return StatusCode(500);
+            }
+        }
+
+        [Route("sdes/decipher")]
+        [HttpPost]
+        public async Task<FileResult> DecipherSDES([FromForm] IFormFile file, [FromForm] KeyHolder key)
+        {
+            try
+            {
+                byte[] bytes;
+                var sdes = new SDES<KeyHolder>();
+
+                if (!KeyHolder.CheckKeyFromFileType(".sdes", key))
+                {
+                    return null;
+                }
+                var name = file.FileName.Split(".");
+                using (var memory = new MemoryStream())
+                {
+                    await file.CopyToAsync(memory);
+                    bytes = memory.ToArray();
+                    List<byte> aux = bytes.OfType<byte>().ToList();
+                }
+                var cipher = Encoding.UTF8.GetString(bytes);
+                var text = sdes.DecryptString(cipher, key);
+                byte[] array = Encoding.UTF8.GetBytes(text);
+                return base.File(array, "text/plain", name[0] + ".txt");
+            }
+            catch
+            {
+                return null;
             }
         }
     }
